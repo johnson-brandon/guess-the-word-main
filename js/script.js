1f0667c01caebb470183e78786f506838fdb00fc
Brandon Johnson <johnsbr99@gmail.com>
2023-09-23 11:38:20 -0600

    GuessButton functionality & clearining input

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const guessedLetters=document.querySelector("ul.guessed-letters"); //UL for guessed letters
 const guessButton=document.querySelector("button.guess");
-const inputLetters=document.querySelector("input#letter.letter"); // letter input
+const lettersInput=document.querySelector("input#letter.letter"); // letter input
 const wordInProgress=document.querySelector("p.word-in-progress"); 
 const remaining=document.querySelector("p.remaining"); //remaining guesses
 const guessesSpan=document.querySelector("span"); // 8 guesses span
@@ -11,11 +11,12 @@ const word="magnolia";
 // console.log(guessedLetters, guessButton, inputLetters, wordInProgress, remaining,
 //     message, guessesSpan, playAgainButton);
 
+// Function to add placeholder text for word to be guessed on screen
 const placeholder=function(word){
     const placeholderLetters=[];
-    console.log(placeholderLetters);
+    // console.log(placeholderLetters);
     for(const letters of word){
-        console.log(letters);
+        // console.log(letters);
         placeholderLetters.push("●");
     }
 
@@ -25,3 +26,16 @@ const placeholder=function(word){
 
 placeholder(word);
 
+
+// Add event listener to guessButton
+guessButton.addEventListener("click", function(e){
+        e.preventDefault();
+        const inputValue=lettersInput.value;
+        console.log(inputValue);
+        clearInput();
+});
+
+//Clear input value for lettersInput
+const clearInput=function(){
+    lettersInput.value = "";
+};  
